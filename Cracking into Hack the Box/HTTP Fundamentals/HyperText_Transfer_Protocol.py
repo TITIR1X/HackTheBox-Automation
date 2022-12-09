6f5f1cacb63cb5bd28bcdb98b6e76bfc01869cb5
Santiago Benavidez <115203597+TITIR1X@users.noreply.github.com>
2022-12-09 20:42:38 -0300

    Update HyperText_Transfer_Protocol.py

--- Cracking into Hack the Box/HTTP Fundamentals/HyperText_Transfer_Protocol.py
+++ Cracking into Hack the Box/HTTP Fundamentals/HyperText_Transfer_Protocol.py
@@ -4,9 +4,10 @@ from OSLogManagement import os_log_parameters, show_last_log
 
 target_ip = input('Paste the Target ip: ') # Example: 46.101.22.232:30201
 
-# Searches for a flag in the contents of the file on the remote server using a specified regular expression                                                                                                         
+# Searches for a flag in the contents of the file on the remote server using a specified regular expression
+# Returns the result as a list                                                                                                    
 flag = os_log_parameters(f'curl {target_ip}/download.php', regex_function='search', regex_parameters='HTB{[a-zA-Z0-9_$!@]+}')
 
 # Convert list to string
-string = ''.join(flag)
-print(string)
+flag = ''.join(flag)
+print(flag)
